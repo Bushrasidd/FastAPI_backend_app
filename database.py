23ca4d17f7e066874db72a2135a95dfb2682d765
Bushra <aayatkhabam391@gmail.com>
2025-11-04 17:55:40 +0530

    Set up database engine and session management; create models file

--- database.py
+++ database.py
@@ -1,13 +1,19 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import sessionmaker, Session, declarative_base
 from dotenv import load_dotenv
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+engine = create_engine(DATABASE_URL)
+
 if DATABASE_URL is not None:
     print(f"Database found:{DATABASE_URL}")
 else:
     print("DATABASE_URL not found in environment variables.")
+
+DBSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
