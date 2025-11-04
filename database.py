30623e11aeb4cba06572978de158a700bf203f3c
Bushra <aayatkhabam391@gmail.com>
2025-11-04 17:32:49 +0530

    Add database connection

---
+++ database.py
@@ -0,0 +1,13 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL is not None:
+    print(f"Database found:{DATABASE_URL}")
+else:
+    print("DATABASE_URL not found in environment variables.")
